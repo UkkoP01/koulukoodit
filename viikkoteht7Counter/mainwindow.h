23e668a533baf029f604c47e656a6abbf7dfbf40
UkkoP01 <ukko.peltola@icloud.com>
2026-02-22 16:39:42 +0200

    Add files via upload

---
+++ viikkoteht7Counter/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    int luku = 0;
+
+private slots:
+
+    void on_count_clicked();
+    void on_reset_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
